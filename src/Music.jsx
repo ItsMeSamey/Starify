69616f3daf1b03c40168e8b504ff0e65d8960842
aayush <aayushgoyaldps@gmail.com>
2024-02-23 23:44:34 +0530

    ads

--- src/Music.jsx
+++ src/Music.jsx
@@ -16,8 +16,42 @@ function Music() {
   const [volume, setVolume] = createSignal(1);
   const [audio, { seek }] = createAudio(source, playing, volume);
   return (
+    
     <div class="flex justify-center items-end box-border w-full h-screen overflow-hidden bg-gray-900">
-      <div class="flex flex-col items-center">
+      <div class="flex flex-col justify-start text-red  hover:text-green-500 top-0 fixed left-0 ml-72 w-[20%] mt-6">
+          <For
+            each={Object.entries({
+              'Sample 1':
+                'https://github.com/solidjs-community/solid-primitives/blob/main/packages/audio/dev/sample1.mp3?raw=true',
+              'Sample 5':
+              'https://github.com/solidjs-community/solid-primitives/blob/main/packages/audio/dev/sample1.mp3?raw=true',
+              'Sample 2':
+                'https://github.com/solidjs-community/solid-primitives/blob/main/packages/audio/dev/sample2.mp3?raw=true',
+                'Sample 10':
+                'https://github.com/solidjs-community/solid-primitives/blob/main/packages/audio/dev/sample2.mp3?raw=true',
+              'Sample 3':
+                'https://github.com/solidjs-community/solid-primitives/blob/main/packages/audio/dev/sample3.mp3?raw=true',
+              'Sample 4':
+                'https://cdn.transistor.fm/file/transistor/m/shows/36181/5f2db453d1bb193bd3c1a3475036f0c7.mp3',
+            })}
+          >
+            {([label, url]) => (
+              <button
+                onClick={() => {
+                  setSource(url);
+                }}
+                class="transition cursor-pointer bg-transparent px-4 py-3 border-none"
+                classList={{
+                  'text-black hover:text-red': url != source(),
+                  'text-white': url == source(),
+                }}
+              >
+                {label}
+              </button>
+            )}
+          </For>
+        </div>
+      <div class="flex flex-col items-center mb-8">
         <div class="flex justify-center shadow items-center bg-black rounded-3xl px-5 py-3 w-[800px]">
           <button
             class="bg-transparent cursor-pointer flex border-none scale-200"
@@ -63,36 +97,6 @@ function Music() {
           </div>
         </div>
 
-        
-        <div class="flex justify-center bg-red-500 rounded-b-xl bg">
-          <For
-            each={Object.entries({
-              'Sample 1':
-                'https://github.com/solidjs-community/solid-primitives/blob/main/packages/audio/dev/sample1.mp3?raw=true',
-              'Sample 2':
-                'https://github.com/solidjs-community/solid-primitives/blob/main/packages/audio/dev/sample2.mp3?raw=true',
-              'Sample 3':
-                'https://github.com/solidjs-community/solid-primitives/blob/main/packages/audio/dev/sample3.mp3?raw=true',
-              'Sample 4':
-                'https://cdn.transistor.fm/file/transistor/m/shows/36181/5f2db453d1bb193bd3c1a3475036f0c7.mp3',
-            })}
-          >
-            {([label, url]) => (
-              <button
-                onClick={() => {
-                  setSource(url);
-                }}
-                class="transition cursor-pointer bg-transparent px-4 py-3 border-none"
-                classList={{
-                  'text-black hover:text-gray-900': url != source(),
-                  'text-red-800': url == source(),
-                }}
-              >
-                {label}
-              </button>
-            )}
-          </For>
-        </div>
       </div>
     </div>
   );
